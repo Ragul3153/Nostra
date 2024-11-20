f061e5c70d37e7b84fbb50a1d9501f8b485921a6
RAGUL S <ragulkumar3153@gmail.com>
2024-11-20 09:34:33 +0530

    Sliding Image secetion

--- index.js
+++ index.js
@@ -20,12 +20,44 @@ closenav.addEventListener("click", function () {
     sidenav.style.right = "-50%"
 })
 
+// Sliding Images //
+
+currentSlideID = 1;
+
+sliderElement = document.getElementById("slider");
+totalSlides = sliderElement.childElementCount;
+console.log(totalSlides)
+
+function next(){
+    if(totalSlides < currentSlideID){
+         currentSlideID++;
+          showSlide()
+    }
+       
+}
+
+function prev(){
+    if(currentSlideID > 1){
+        currentSlideID--;
+         showSlide()
+    }
+       
+}
 
 
+function showSlide(){
+    slides = document.getElementById("slider").getElementsByTagName("img")
+    for(let index=0; index < totalSlides; index++) {
+        const element = slides[index];
+        if(currentSlideID===index+1){
+            element.classList.remove("hidden")
+        }
 
-
-
-
+        else{
+            element.classList.add("hidden")
+        }
+    }
+}
 
 
 
